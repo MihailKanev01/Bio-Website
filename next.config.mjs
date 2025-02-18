326b154bd4d342665efc51c234558670a4d22913
MihailKanev01 <mihail.kanev.1995@abv.bg>
2025-02-18 17:07:16 +0200

    Small changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export", // Enables static export
-    basePath: "/your-repo-name", // Replace with your GitHub repo name
+    basePath: "/Bio-Website", // Must match your GitHub repo name
     images: { unoptimized: true }, // Fix images in static export
   };
   
-  module.exports = nextConfig;
+  export default nextConfig;
   
